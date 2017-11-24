4a51860da0a1746aa50eaafc7c6773757b423aea
Adel <adel@liwio.com>
2017-11-24 14:59:08 +0100

    first depot fmwk

---
+++ sdkCRM.framework/Headers/sdkCRM.h
@@ -0,0 +1,19 @@
+//
+//  sdkCRM.h
+//  sdkCRM
+//
+//  Created by Adel on 23/11/2017.
+//  Copyright © 2017 Alias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sdkCRM.
+FOUNDATION_EXPORT double sdkCRMVersionNumber;
+
+//! Project version string for sdkCRM.
+FOUNDATION_EXPORT const unsigned char sdkCRMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdkCRM/PublicHeader.h>
+
+
